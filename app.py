a40d644565038d95c2fba57de4029fcd4a6fad3b
workspace-nuts12 <153623778+workspace-nuts12@users.noreply.github.com>
2024-02-26 23:54:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -166,6 +166,6 @@ def dated_url_for(endpoint, **values):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get('PORT', 8080))
-    # app.run(host ='0.0.0.0',port = port)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
